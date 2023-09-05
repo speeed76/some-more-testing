fd8ef3d44890ffd6ac9a92bc096967f6329819ed
Pawel Giers <pawel.giers@gmail.com>
2023-09-05 23:07:05 +0000

    create login.js

---
+++ login.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+module.exports = () => {
+  router.post('/signup', ExpressRouterAdapter.adapt(new SignUpRouter()));
+};
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      };
+      const httpResponse = await router.route(httpRequest);
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    };
+  }
+}
+
+// Presentation Layer
+// singup-router.js
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body;
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword);
+    return {
+      statusCode: 200,
+      body: user
+    };
+  }
+}
+
+// Domain Layer
+// signup-usecase.js
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password);
+    }
+  }
+}
+
+// Infra Layer
+// add-account-repository.js
+const mongoose = require('mongoose');
+const AccountModel = mongoose.model('Account');
+class AddAccountRepository {
+  async add (email, password, repeatPassword) {
+    const user = await AccountModel.create({ email, password });
+    return user;
+  }
+}
